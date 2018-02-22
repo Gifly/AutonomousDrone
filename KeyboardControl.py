0476bde8411d4d5efbde8cccfc93cad8a511aefd
Paul Vazquez <pev@live.com.mx>
2018-02-21 21:58:13 -0600

    Agregue la funcion getImage para recibir un frame del drone

--- KeyboardControl.py
+++ KeyboardControl.py
@@ -1,93 +1,105 @@
 ##################################################################################################
 ###### Playground																			######
 ##################################################################################################
-if __name__ == "__main__":
 ###
 ### Here you can write your first test-codes and play around with them
 ###
+import time
+from api import ps_drone
+import pygame
+import cv2
 
-	import time
-	from api import ps_drone
-	import pygame
-	import cv2
+def usarVideo():
+	print "Mostrar video"
+	IMC = 	 drone.VideoImageCount	
+	while drone.VideoImageCount==IMC: time.sleep(0.01)	# Wait until the next video-frame
+	IMC = drone.VideoImageCount
+	#key = drone.getKey()
+	#if key:		stop = True
+	img  = drone.VideoImage					# Copy video-image
+	pImg = cv2.resize(img,(360,640))
+	return img		# Process video-image
+						
 
-	def mostrarImgF():
-		img = drone.VideoImage
-		img = cv2.resize(img, (400,100))
-		cv2.imshow('Foto tomada por el drone', img)
-		drone.hover()
-		cv2.waitKey(1)
-		cv2.destroyAllWindows()
-	
-	pygame.init()
-	screen = pygame.display.set_mode((320, 240))
-	drone = ps_drone.Drone()								# Start using drone					
-	drone.startup()
-	drone.reset()
-	print "Drone inicializado"
-	drone.setConfigAllID()
-	print "Proceso de configuracion"
-	drone.hdVideo()
-	drone.frontCam()
-	drone.startVideo()
-	print "Setting up the video options"
-	time.sleep(2)
-	camFron = True
-	stop = False
-	while not stop:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
+pygame.init()
+screen = pygame.display.set_mode((320, 240))
+drone = ps_drone.Drone()								# Start using drone					
+drone.startup()
+drone.reset()
+drone.trim()                                       # Recalibrate sensors
+drone.getSelfRotation(5)                           # Get auto-alteration of gyroscope-sensor
+print "Drone inicializado"
+drone.setConfigAllID()
+print "Proceso de configuracion"
+drone.sdVideo()
+drone.frontCam()
+drone.startVideo()
+CDC = drone.ConfigDataCount
+while CDC == drone.ConfigDataCount:	time.sleep(0.0001)	# Wait until it is done (after resync is done)
+drone.startVideo()					# Start video-function
+print "Setting up the video options"
+time.sleep(2)
+camFron = True
+stop = False
+mostrarImg = False
+print "Listo para recibir comandos"
+while not stop:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			stop=True
+		elif event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_ESCAPE:
+				pygame.display.quit()
 				pygame.quit()
 				stop=True
-			elif event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_ESCAPE:
-					pygame.display.quit()
-					pygame.quit()
-					stop=True
-				elif event.key == pygame.K_w:
-					drone.moveForward()
-				elif event.key == pygame.K_s:
-					drone.moveBackward()
-				elif event.key == pygame.K_a:
-					drone.moveLeft()
-				elif event.key == pygame.K_d:
-					drone.moveRight()
-				elif event.key == pygame.K_q:
-					drone.turnLeft(	)
-				elif event.key == pygame.K_e:
-					drone.turnRight()
-				elif event.key == pygame.K_UP:
-					drone.moveUp()
-				elif event.key == pygame.K_DOWN:
-					drone.moveDown()
-				elif event.key == pygame.K_RETURN:
-					print "up"
-					drone.takeoff()
-				elif event.key == pygame.K_SPACE:
-					drone.land()
-				elif event.key == pygame.K_p:
-					mostrarImgF()
-				elif event.key == pygame.K_v:
-					print "video encendido"
-					drone.startVideo()
-					drone.showVideo()
-					mostrarImg=True
+			elif event.key == pygame.K_w:
+				drone.moveForward()
+			elif event.key == pygame.K_s:
+				drone.moveBackward()
+			elif event.key == pygame.K_a:
+				drone.moveLeft()
+			elif event.key == pygame.K_d:
+				drone.moveRight()
+			elif event.key == pygame.K_q:
+				drone.turnLeft(	)
+			elif event.key == pygame.K_e:
+				drone.turnRight()
+			elif event.key == pygame.K_UP:
+				drone.moveUp()
+			elif event.key == pygame.K_DOWN:
+				drone.moveDown()
+			elif event.key == pygame.K_RETURN:
+				print "up"
+				drone.takeoff()
+			elif event.key == pygame.K_SPACE:
+				drone.land()
+			elif event.key == pygame.K_p:
+				mostrarImgF()
+			elif event.key == pygame.K_v:
+				mostrarImg=True
+				print "video encendido"
+			elif event.key == pygame.K_t:
+				if(camFron):
+					drone.groundCam()
+					camFron= not camFron
+				else:
+					drone.frontCam()
+		elif event.type == pygame.KEYUP:
+			drone.hover()
+	if(mostrarImg):
+		pImg = usarVideo()
+		print "Muestro la imagen actual"
+		cv2.imshow('Drones video',pImg)			# Show processed video-image
+		stop = (cv2.waitKey(1) ==13)		#bat = drone.getBattery()[0]
+	#print bat
+	#drone.showVideo(False)
+	#f = pygame.font.Font(None, 20)
+	#hud = f.render('Battery: %i%%' % bat, True, (255,0,0))
+	#screen.blit(hud,(10,10))
+cv2.destroyAllWindows()
+pygame.display.quit()
+pygame.quit()
+sys.exit()
 
-				elif event.key == pygame.K_t:
-					if(camFron):
-						drone.groundCam()
-					else:
-						drone.frontCam()
-			elif event.type == pygame.KEYUP:
-				drone.hover()
-		#bat = drone.getBattery()[0]
-		#print bat
-		drone.showVideo(False)
-		#f = pygame.font.Font(None, 20)
-		#hud = f.render('Battery: %i%%' % bat, True, (255,0,0))
-		#screen.blit(hud,(10,10))
-	pygame.display.quit()
-	pygame.quit()
-	sys.exit()
 
-	
