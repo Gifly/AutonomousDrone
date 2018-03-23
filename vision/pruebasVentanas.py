38ec324f1739f09298e365d30641da3686559942
Paul Vazquez <pev@live.com.mx>
2018-03-22 21:55:52 -0600

    Frames mision with yellow indicators first try

---
+++ vision/pruebasVentanas.py
@@ -0,0 +1,40 @@
+import cv2
+import numpy as np
+import sys
+import time
+import vision
+sys.path.insert(0, '../')
+import api.ps_drone as ps_drone
+def getImage():
+    IMC = drone.VideoImageCount 
+    while drone.VideoImageCount==IMC: time.sleep(0.01)  # Wait until the next video-frame
+    img  = drone.VideoImage 
+    pImg = cv2.resize(img,(640, 360), interpolation = cv2.INTER_CUBIC)               
+    return pImg      # Returns image
+
+print "Booting up the drone for frames testing"
+drone = ps_drone.Drone()                                                    
+drone.startup()
+drone.reset()
+drone.trim()                                     
+drone.getSelfRotation(5) 
+drone.setConfigAllID()
+cap = cv2.VideoCapture(0)
+#Drone's camera initial configuration
+print "Booting up the camera"
+drone.frontCam()
+drone.hdVideo()
+drone.startVideo()
+CDC = drone.ConfigDataCount
+while CDC == drone.ConfigDataCount: time.sleep(0.0001)  # Wait until it is done (after resync is done)
+drone.startVideo()
+vision.setRange()
+k=0
+x = y = 0
+while k != 27:
+	frame =  getImage()
+	x , y, area, frame = vision.getVentana(frame)
+	print "Area: ", area , "Centro: ", x , y
+	cv2.imshow("Ventana mas grande", frame)
+	k =cv2.waitKey(5)%256
+cv2.destroyAllWindows()
