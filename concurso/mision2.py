c25a6be2991d98de8f6e3a410f80acf0266a2cc0
Alex <alexhor22@gmail.com>
2018-03-22 23:57:57 -0600

    Updated mision 2

--- concurso/mision2.py
+++ concurso/mision2.py
@@ -1,117 +1,118 @@
-import VL53L0X
+import cv2
 import sys
 sys.path.insert(0,"../")
 import api.ps_drone as ps_drone
 from tools import emergency
+from vision import vision
+from PID import PIDrone
 import time
+
+drone = ps_drone.Drone()
+
+def getImage():
+    IMC = drone.VideoImageCount 
+    while drone.VideoImageCount==IMC: time.sleep(0.01)  # Wait until the next video-frame
+    img  = drone.VideoImage                 # Copy video-image
+    pImg = cv2.resize(img,(640,360), interpolation = cv2.INTER_CUBIC)
+    return img      # Returns image
 
 def main():
 
-    drone = ps_drone.Drone()  # Start using drone
-    tof = VL53L0X.VL53L0X()
+      # Start using drone
+
     thread = emergency.keyThread(drone)
-    tof.start_ranging(4)
-    time.sleep(0.001)
-    if(tof.get_distance()==-1):
-	print "No se inicializo correctamente el tof"
-	return 	   
     print "Configuracion del drone"
     drone.startup()  # Connects to drone and starts subprocesses
     drone.reset()  # Always good, at start
     drone.trim()                                       # Recalibrate sensors
-    #drone.getSelfRotation(5)
+    drone.getSelfRotation(5)
     time.sleep(0.5)
     thread.start()
+
+    #Drone's camera initial configuration
+    print "Booting up the camera"
+    drone.frontCam()
+    drone.hdVideo()
+    drone.startVideo()
+    CDC = drone.ConfigDataCount
+    while CDC == drone.ConfigDataCount: time.sleep(0.0001)  # Wait until it is done (after resync is done)
+    drone.startVideo()
     print "BATERIA ACTUAL: ", drone.getBattery()[0]
-
-    #NAV DATA
-    print "Obteniendo paquete de medidas"
-    drone.useDemoMode(False)
-    drone.getNDpackage(["demo","pressure_raw","altitude","magneto","wifi"])
-
+    PIDx = PIDrone.DronePID(0.050, 0.02, 0)
 
     print "Comienzo el programa"
+
+    time.sleep(0.001)
+    drone.useDemoMode(False)
+    drone.getNDpackage(["demo","pressure_raw","altitude","magneto","wifi"])
+    time.sleep(1.0)
+
     print "takeoff"
     drone.takeoff()
     time.sleep(2)
-    drone.hover()
-    time.sleep(2)
-    #drone.setSpeed(0.1)
     print "hovering"
-    
-    #THIS PART GETS NEAR THE OBSTACLE
-    distance = tof.get_distance()
-    print "Distance before while: ",distance
-    while distance > 1300 and distance!=0:
-        distance = tof.get_distance()
-        print "Distance: ", distance
-        if distance < 1301:
-            drone.moveBackward(0.5)
-        else:
-            drone.moveForward(0.1)
-        print "next"
-    #time.sleep(3)
-    print "back"
-    drone.moveBackward(0.5)
-    time.sleep(0.5)
-    print "hover"
     drone.hover()
-    time.sleep(5)
+    time.sleep(4)
+    #drone.setSpeed(0.1)
+   
+
+    #GETS NEAR THE OBJECT
+    vision.setRange()
+    frame = getImage()
+    coordX, coordY, area = vision.getCenter(frame)
+    while coordY > -1 and coordY < 300 and area < 700000:
+        drone.moveForward(0.15)
+        frame = getImage()
+        coordX, coordY, area = vision.getCenter(frame)
+        print coordX, coordY, area
+    drone.moveBackward(0.2)
+    time.sleep(1)
+
+
+    #ALINENANDOSE CON EL OBJETO
+    drone.hover()
+    time.sleep(1)
+    print "Alinenandose"
+    initTime = time.time()
+    while(time.time()< initTime + 2):
+        frame = getImage()
+        coordX, coordY, area = vision.getCenter(frame)
+        if coordX != -1 and coordY !=-1:
+            SpeedX = -1.0*PIDx.getVelocity(0.005,320,coordX)
+            drone.move(SpeedX, 0.0, 0.0, 0.0)
+            print SpeedX
+        else:
+            drone.hover()
+            print "Not found again"
+        
+    drone.stop()
+    time.sleep(1)
+
+
 
     #THIS PART GOES UP 1600 mm
     NDC = drone.NavDataCount
     alti = 0.0
-    while alti < 1400:
+    while alti < 1500:
         while drone.NavDataCount == NDC:   time.sleep(0.001)
         NDC = drone.NavDataCount
         alti = drone.NavData["altitude"][3]
         print "Altitude: " + str(alti)
-        drone.move(0,0.05,0.99,0.0)
+        drone.move(0,-0.05,0.99,0.0)
 
-    # #THIS PART GOES UP WHILE SEEIONG THE OBSTACLE AND GOES FORWARD
-    # #WITH A TIME OUT
-    # thisTime = time.time()
-    # distance = tof.get_distance()
-    # while distance < 5000:
-    #     print  "Distancia subiendo ", distance
-    #     drone.moveUp(0.7)
-    #     distance = tof.get_distance()
-    #     actualTime = time.time()
-    #     if (actualTime - thisTime) > 7 : 
-    #         drone.land()
-    #         print "TIME OUT"
-    #         break
-    # print "FINISHED GOING UP"
+
+
 
     drone.moveDown(0.2)
     time.sleep(0.5)
     drone.hover()
     time.sleep(2)	
 
-    #THIS LANDS THE DRONE AFTER THE TIME OUT OR WHEN
-    #IT DETECTS A WALL
-    thisTime = time.time()
-    distance = tof.get_distance()
-    print "Distance before while: ",distance
-    while distance > 1000 or distance < 1:
-        distance = tof.get_distance()
-        print "Distance: ",distance
-        if distance <  1001:
-            drone.moveBackward(0.5)
-        else:
-            drone.moveForward(0.1)
-        actualTime = time.time()
-        if (actualTime - thisTime) > 6 : 
-            drone.land()
-            print "TIME OUT"
-            break
- 
-    print "back"
-    drone.moveBackward(0.5)
-    time.sleep(0.5)
-    drone.hover()
-    time.sleep(1)
+    drone.moveForward(0.15)
+    time.sleep(2)
 
+    drone.hover()
+    time.sleep(2)
     print "land"
     drone.land()
 
